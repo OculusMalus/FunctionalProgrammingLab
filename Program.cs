a0f006e18bb15c8546211a9495719e339d07f889
OculusMalus <stphn70@gmail.com>
2016-09-09 15:18:03 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Operators
+{
+    class LINQQueryExpressions
+    {
+        static void Main()
+        {  
+            string s = "90, 100, 82, 89, 55, 75, 88, 49, 32";
+
+            string[] words = s.Split(',');
+
+            int length = words.Length;
+
+            int[] scores = new int[words.Length];
+
+            for (int i=0; i< words.Length; i++)
+            {
+                int number = Int32.Parse(words[i]);
+                scores[i] = number;
+            }
+            
+            // Define the query expression.
+            IEnumerable<int> scoreQuery = from score in scores
+                                          orderby score 
+                                          //where score > 50
+                                          select score;
+
+            // Execute the query.
+            Console.Write("Here is a full list of scores:\n");
+            foreach (int i in scoreQuery)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
